e5d622c7468caf2b4c8b70b7b70f0557eed6d46d
dino <irvin.em@live.com>
2019-11-25 15:12:22 +0800

    modify lesson5

--- lesson5/lesson5.go
+++ lesson5/lesson5.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 )
 
-var(
+var (
 	webEng *gin.Engine
 )
 
-func main(){
+func main() {
 	webEng = gin.Default()
 
 	//处理 带必选参数的路由
@@ -29,7 +30,7 @@ func main(){
 	/article/id		yes
 	/article/id/	yes	(redirecting request 301: /article/id/ --> /article/id)
 */
-func NecessaryParams(c *gin.Context){
+func NecessaryParams(c *gin.Context) {
 	searchBy := c.Param("searchBy")
 	c.String(http.StatusOK, "Necessary=> search article by %s", searchBy)
 }
@@ -41,13 +42,22 @@ func NecessaryParams(c *gin.Context){
 	/article/id/2159 		yes
 	/article/id/2159/other 	yes
 */
-func OptionalParams(c *gin.Context){
+func OptionalParams(c *gin.Context) {
+	RangeParams(c.Params)
+
 	condition := c.Param("condition")
 	searchBy := c.Param("searchBy")
-	c.String(http.StatusOK, "Optional=> search article by %s = %s",searchBy,condition)
+	c.String(http.StatusOK, "Optional=> search article by %s = %s", searchBy, condition)
 }
 
 // 处理其他没有定义的路由，自定义404
-func Others(c *gin.Context){
+func Others(c *gin.Context) {
 	c.String(http.StatusNotFound, "No handler for => %s", c.Request.RequestURI)
+}
+
+//Context.Params 是一个key-value结构的数组，自己可以单独处理
+func RangeParams(p gin.Params) {
+	for k, v := range p {
+		log.Printf("\t %d : %s = %s\n", k, v.Key, v.Value)
+	}
 }
